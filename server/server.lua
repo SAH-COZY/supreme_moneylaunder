1b4d2a45ad137140b81ead9efb98bf1f9768e71e
SAHCOZY <58663587+SAH-COZY@users.noreply.github.com>
2021-08-21 20:49:37 +0200

    Update server.lua
    fixed exploitable event

--- server/server.lua
+++ server/server.lua
@@ -6,7 +6,7 @@ RegisterServerEvent('supreme_moneylaunder:GetCash')
 AddEventHandler('supreme_moneylaunder:GetCash', function(itemName, price)
 	local xPlayer  = ESX.GetPlayerFromId(source)
         local randomLaunder = math.random(1000,2000)
-		if xPlayer.getAccount('black_money').money > 0 then
+		if xPlayer.getAccount('black_money').money >= randomLaunder then
 			xPlayer.removeAccountMoney('black_money', randomLaunder)
 			xPlayer.addMoney(randomLaunder)
 			TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'You laundered $' .. randomLaunder, })
